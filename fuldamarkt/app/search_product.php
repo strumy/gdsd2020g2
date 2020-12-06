f1041830429e2e79963b472664e6016280ae0b28
Ahmed-RA <52761735+Ahmed-RA@users.noreply.github.com>
2020-12-06 07:08:49 +0100

    Search script using mysqli connection
    Make sure mysqli module is enabled in the php.ini file. Returns result as a 2d array.

--- fuldamarkt/app/search_product.php
+++ fuldamarkt/app/search_product.php
@@ -3,27 +3,40 @@
 require '../project.php';
 
 //This function should return an array of results. I am not sure how the twig template would handle the array :/
-function get_product($db, $errors, $search_by_data)
+function get_product($errors, $search_by_data)
 {
     $executed = false;
-
-    if (!isset($search_by_data['text_input_field'])) {
-        //I do not know how to fix the syntax error below :(
-        $sql_query = "SELECT title, market_category, price FROM Fulda_Markt1.MARKET_TABLE WHERE market_category='$search_by_data['category']' ORDER BY $search_by_data['sort_by'] ASC LIMIT 10;";
-        else{
-            $sql_query = "SELECT title, market_category, price FROM Fulda_Markt1.MARKET_TABLE WHERE TITLE LIKE '%$search_by_data['text_input_field']' AND market_category='$search_by_data['category']' ORDER BY $search_by_data['sort_by'] ASC LIMIT 10;";
+    $text_input = $search_by_data['text_input'];
+    $category = $search_by_data['category'];
+    $sort_by = $search_by_data['sort_by'];
+    //change these accordingly **************
+    $dbhost = '192.168.178.161:3306'; 
+    $dbuser = 'admin1gdsd'; 
+    $dbpass = 'wordsthatpassdb';
+    //change these accordingly **************
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+   
+    if(!$conn ){
+        die('Could not connect: ' . mysqli_error());
+        $errors[] = "Search query failed: " . $ex->getMessage();
+        return $executed;
         }
-    }
+         
+    mysqli_select_db($conn, 'fuldamarkt_proddb');    
+    $sql_query = "SELECT * FROM MARKET_TABLE WHERE TITLE LIKE '%$text_input%' AND market_category='$category' ORDER BY $sort_by ASC LIMIT 10;";
+    
 
     try {
-        $query_result = $db->query($sql_query)
+        $query_result = mysqli_query($conn, $sql_query);
        // $executed = $query->execute(true);
     } catch (\Exception $ex) {
         $errors[] = "Search query failed: " . $ex->getMessage();
+        mysqli_close($conn);
         return $executed;
     }
-
-    return $query_result;
+    mysqli_close($conn);
+    $query_result2 = $query_result->fetch_all(MYSQLI_ASSOC);
+    return $query_result2;
 }
 
 
@@ -39,14 +52,14 @@ $hasError = false;
 
 if ($request->getMethod() == "POST") {
     $search_by_data = array(
-        'text_input_field' => trim($request->get('text_input_field')),
-        'category' => trim($request->get('category')), //should have a default value in the drop down menu
+        'text_input' => trim($request->get('text_input')),
+        'category' => trim($request->get('categories')), //should have a default value in the drop down menu
         'sort_by' => trim($request->get('sort_by'))    //should have a default value in the drop down menu
     );
 
     
 
-$search_data = get_product($db, $errors, $search_by_data);
+$search_data = get_product($errors, $search_by_data); //this is a 2d array with index starting at 0 for first row
 }
 /* Setting Template Variable, $page_data */
 $page_data = ['title' => $title, 'body' => $body, 'errors'=> $errors, 'message' => $message, 'result' => $search_data];
