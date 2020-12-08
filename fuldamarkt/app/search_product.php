f8c8d3401d7dceb13e0015e36825d577de429757
Ahmed-RA <52761735+Ahmed-RA@users.noreply.github.com>
2020-12-08 06:18:35 +0100

    modified search_product to use $dbi variable

--- fuldamarkt/app/search_product.php
+++ fuldamarkt/app/search_product.php
@@ -3,38 +3,33 @@
 require '../project.php';
 
 //This function should return an array of results. I am not sure how the twig template would handle the array :/
-function get_product($errors, $search_by_data)
+function get_product($dbi, $errors, $search_by_data)
 {
     $executed = false;
     $text_input = $search_by_data['text_input'];
     $category = $search_by_data['category'];
     $sort_by = $search_by_data['sort_by'];
-    //change these accordingly **************
-    $dbhost = '192.168.178.161:3306'; 
-    $dbuser = 'admin1gdsd'; 
-    $dbpass = 'wordsthatpassdb';
-    //change these accordingly **************
-    $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+
    
-    if(!$conn ){
+    if(!$dbi ){
         die('Could not connect: ' . mysqli_error());
         $errors[] = "Search query failed: " . $ex->getMessage();
         return $executed;
         }
          
-    mysqli_select_db($conn, 'fuldamarkt_proddb');    
+    mysqli_select_db($dbi, 'fuldamarkt_proddb');    
     $sql_query = "SELECT * FROM MARKET_TABLE WHERE TITLE LIKE '%$text_input%' AND market_category='$category' ORDER BY $sort_by ASC LIMIT 10;";
     
 
     try {
-        $query_result = mysqli_query($conn, $sql_query);
+        $query_result = mysqli_query($dbi, $sql_query);
        // $executed = $query->execute(true);
     } catch (\Exception $ex) {
         $errors[] = "Search query failed: " . $ex->getMessage();
-        mysqli_close($conn);
+        
         return $executed;
     }
-    mysqli_close($conn);
+    
     $query_result2 = $query_result->fetch_all(MYSQLI_ASSOC);
     return $query_result2;
 }
@@ -46,6 +41,7 @@ function get_product($errors, $search_by_data)
 $temp_name = 'market.twig';
 $title = 'Market Page';
 $body = 'Search for items';
+$errors = '';
 $message = '';
 $hasError = false;
 // $user = false;
@@ -59,11 +55,15 @@ if ($request->getMethod() == "POST") {
 
     
 
-$search_data = get_product($errors, $search_by_data); //this is a 2d array with index starting at 0 for first row
+$search_data = get_product($dbi, $errors, $search_by_data); //this is a 2d array with index starting at 0 for first row
 }
 /* Setting Template Variable, $page_data */
+if(isset($search_data)){
 $page_data = ['title' => $title, 'body' => $body, 'errors'=> $errors, 'message' => $message, 'result' => $search_data];
-
+}
+else{
+    $page_data = ['title' => $title, 'body' => $body, 'errors'=> $errors, 'message' => $message];
+}
 try {
     echo $template->render($temp_name, $page_data);
 } catch (\Exception $ex) {
