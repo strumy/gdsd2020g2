008e47771926b0a7e660f41eb0b84abe49c38fd3
Ahmed-RA <52761735+Ahmed-RA@users.noreply.github.com>
2020-12-27 15:34:25 +0100

    See description
    1. Input validation (alphanumeric and less than 40 chars.).
    2. More sorting options

--- fuldamarkt/search_product.php
+++ fuldamarkt/search_product.php
@@ -2,7 +2,7 @@
 
 require 'project.php';
 
-//This function should return an array of results. 
+//This function should return an associative array of results. 
 function get_product($dbi, $errors, $search_by_data)
 {
     $executed = false;
@@ -10,16 +10,47 @@ function get_product($dbi, $errors, $search_by_data)
     $category = $search_by_data['category'];
     $sort_by = $search_by_data['sort_by'];
 
-   
+    //Input validation to check if input is alphanumeric and less than 40 characters
+    if(!empty($text_input)){
+        if(!ctype_alnum(str_replace(' ','',$text_input)) || (strlen($text_input)) >= 40){
+            return 1;
+        }
+    }
+    
+    //check database connection
     if(!$dbi ){
         die('Could not connect: ' . mysqli_error());
         $errors[] = "Search query failed: " . $ex->getMessage();
         return $executed;
         }
          
-    mysqli_select_db($dbi, 'fuldamarkt_proddb');    
-    $sql_query = "SELECT * FROM MARKET_TABLE WHERE TITLE LIKE '%$text_input%' AND market_category LIKE '%$category%' ORDER BY $sort_by ASC LIMIT 10;";
-    
+    mysqli_select_db($dbi, 'fuldamarkt_proddb'); 
+
+
+    // check sorting to see whether to sort by ascending or descending order for price and date sorting
+    if(strpos($sort_by,"ascending") !== false){
+        if(strpos($sort_by,"Price") !== false){
+            $sort_by = "Price"; //formating 
+        }
+        if(strpos($sort_by,"date") !== false){
+            $sort_by = "date_inserted";
+        }  
+        $sql_query = "SELECT * FROM MARKET_TABLE WHERE STATUS='available' AND TITLE LIKE '%$text_input%' AND market_category LIKE '%$category%' ORDER BY $sort_by ASC;";
+    }
+    else if(strpos($sort_by,"descending") !== false){ 
+        if(strpos($sort_by,"Price") !== false){
+            $sort_by = "Price";
+        }
+        if(strpos($sort_by,"date") !== false){
+            $sort_by = "date_inserted";
+        }   
+        $sql_query = "SELECT * FROM MARKET_TABLE WHERE STATUS='available' AND TITLE LIKE '%$text_input%' AND market_category LIKE '%$category%' ORDER BY $sort_by DESC;";
+    }
+    else{
+        $sql_query = "SELECT * FROM MARKET_TABLE WHERE STATUS='available' AND TITLE LIKE '%$text_input%' AND market_category LIKE '%$category%' ORDER BY $sort_by ASC;";
+    }
+
+
 
     try {
         $query_result = mysqli_query($dbi, $sql_query);
@@ -40,11 +71,14 @@ function get_product($dbi, $errors, $search_by_data)
 
 $temp_name = 'market.twig';
 $title = 'Market Page';
-$body = 'Search for items';
+$body = '';
 $errors = '';
 $message = '';
-$hasError = false;
-// $user = false;
+$search_data = array();
+$search_data_pictures = array(); /*array containing multiple entries of (directory:images), the directory is a string containg the location of a posts' images.
+the ['images'] is a subarray containing names of pictures inside the directory */
+
+
 
 if ($request->getMethod() == "POST") {
     $search_by_data = array(
@@ -56,22 +90,28 @@ if ($request->getMethod() == "POST") {
     
 
 $search_data = get_product($dbi, $errors, $search_by_data); //this is a 2d array with index starting at 0 for first row
-$search_data_pictures = array(); /*array containing multiple entries of (directory:images), the directory is a string containg the location of a posts' images.
-the ['images'] is a subarray containing names of pictures inside the directory */
 
 
-if(isset($search_data)){ //check if there are results to the search query
-    foreach($search_data as $val){ 
-        if(isset($val['picture'])){ //check if a post has a picture directory
-            if(file_exists($_SERVER['DOCUMENT_ROOT'] . $val['picture'])){ 
-                $files = array_diff( scandir( $_SERVER['DOCUMENT_ROOT'] . $val['picture']), array(".", "..") ); //get list of image files found in the post's directory
-                $temp_array = array('directory' => $val['picture'],'images' => $files);  
-                array_push($search_data_pictures, $temp_array);
+if($search_data != 1){
+    if(!empty($search_data)){ //check if there are results to the search query
+        foreach($search_data as $val){ 
+           if(isset($val['picture'])){ //check if a post has a picture directory
+              if(file_exists($_SERVER['DOCUMENT_ROOT'] . $val['picture'])){ 
+                  $files = array_diff( scandir( $_SERVER['DOCUMENT_ROOT'] . $val['picture']), array(".", "..") ); //get list of image files found in the post's directory
+                  $temp_array = array('directory' => $val['picture'],'images' => $files);  
+                  array_push($search_data_pictures, $temp_array);
+              }
             }
         }
+    unset($val);
     }
-unset($val);
+    else{
+    $message = "No Data Found";
+    }
 }
+else{
+$message = "Search input needs to be alphanumeric and less than 40 characters";
+}    
 }
 
 // necessary to forward to the twig template so that the "create post" button only appears when a user is logged in
@@ -79,13 +119,12 @@ $check_auth = $session->get('is_authenticated');
 
 
 /* Setting Template Variable, $page_data */
-if(isset($search_data)){
+if(!empty($search_data)){
 $page_data = ['title' => $title, 'body' => $body, 'errors'=> $errors, 'message' => $message, 'result' => $search_data,
-  'search_data_pictures' => $search_data_pictures, 'Doc_root' => $_SERVER['DOCUMENT_ROOT'], 'check_auth' => $check_auth];
+  'search_data_pictures' => $search_data_pictures, 'Doc_root' => $_SERVER['DOCUMENT_ROOT'], 'check_auth' => $check_auth, 'session' => $session];
 }
-else{
-    $message = "No Data Found";
-    $page_data = ['title' => $title, 'body' => $body, 'errors'=> $errors, 'message' => $message, 'check_auth' => $check_auth];
+else{   
+    $page_data = ['title' => $title, 'body' => $body, 'errors'=> $errors, 'message' => $message, 'check_auth' => $check_auth, 'session' => $session];
 }
 try {
     echo $template->render($temp_name, $page_data);
